94f716a95ef8b819561c5b51fd92c46260af89a7
Hanaif <hanaif1@gmail.com>
2020-05-16 18:45:29 +0100

    Simple React App
    Simple React App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
     return ( 
         <div>
-            <p>Hello</p>
+            <h1>Hello!</h1>
+            <p> Welcome To My Simple React App</p>
         </div>
     );
 }
